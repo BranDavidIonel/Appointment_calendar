0712d3a9797d7508d8bf7901af4db711948a3684
david <bran.david.ionel@gmail.com>
2022-02-08 12:18:11 +0200

    add migrations file , install livewire

---
+++ resources/views/livewire/appointments.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <h2>All Appointments</h2>
+        </div>
+        <div class="col-md-6">
+            <h2>New Appointment</h2>
+        </div>
+    </div>
+</div>
